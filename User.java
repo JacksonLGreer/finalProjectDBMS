123f43eb3b7faad6e17ba67493279e0195820a57
miro-177 <102875255+miro-177@users.noreply.github.com>
2025-05-01 15:09:36 -0400

    Added a User model

---
+++ User.java
@@ -0,0 +1,60 @@
+package uga.edu.cs.finalProjectDBMS.Models;
+
+public class User {
+    private int userId;
+    private String email;
+    private String passwordHash;
+    private String firstName;
+    private String lastName;
+
+    // Constructors
+    public User() {}
+
+    public User(String email, String passwordHash, String firstName, String lastName) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters and Setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
